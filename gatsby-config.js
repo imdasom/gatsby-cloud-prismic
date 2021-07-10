c84b7f8153d83b9c2499770a8fd52a3a5b9b8896
imdasom <imdasom101@gmail.com>
2021-07-10 16:46:17 +0900

    쿼리에러 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,11 @@ module.exports = {
     title: 'team-wired',
   },
   plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
